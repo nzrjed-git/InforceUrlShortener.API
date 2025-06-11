5eabcf430185b1b9b9d5fec52096909032c29ebe
Nazar <nazar.sachuk.dev@gmail.com>
2025-06-11 11:21:55 +0300

    feat: add logging and add validation to the CreateShortenedUrlCommand

--- InforceUrlShortener.API/Middlewares/ErrorHandlingMiddleware.cs
+++ InforceUrlShortener.API/Middlewares/ErrorHandlingMiddleware.cs
@@ -2,7 +2,8 @@
 
 namespace InforceUrlShortener.API.Middlewares
 {
-    public class ErrorHandlingMiddleware() : IMiddleware
+    public class ErrorHandlingMiddleware(
+        ILogger<ErrorHandlingMiddleware> logger) : IMiddleware
     {
         public async Task InvokeAsync(HttpContext context, RequestDelegate next)
         {
@@ -19,19 +20,23 @@ namespace InforceUrlShortener.API.Middlewares
             {
                 context.Response.StatusCode = 404;
                 await context.Response.WriteAsync(ex.Message);
+                logger.LogError(ex.Message);
             }
             catch (NotFoundByShortCodeException ex)
             {
                 context.Response.StatusCode = 404;
                 await context.Response.WriteAsync(ex.Message);
+                logger.LogError(ex.Message);
             }
             catch (OriginalUrlDuplicateException ex)
             {
                 context.Response.StatusCode = 409;
                 await context.Response.WriteAsync(ex.Message);
+                logger.LogError(ex.Message);
             }
             catch (Exception ex)
             {
+                logger.LogError(ex, ex.Message);
                 context.Response.StatusCode = 500;
                 await context.Response.WriteAsync("Something went wrong");
             }
---
+++ InforceUrlShortener.Application/ShortenedUrls/Commands/CreateShortenedUrl/CreateShortenedUrlCommandValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+
+namespace InforceUrlShortener.Application.ShortenedUrls.Commands.CreateShortenedUrl
+{
+    public class CreateShortenedUrlCommandValidator : AbstractValidator<CreateShortenedUrlCommand>
+    {
+        public CreateShortenedUrlCommandValidator()
+        {
+            RuleFor(c => c.OriginalUrl)
+                .NotEmpty().WithMessage("Original url is required.")
+                .Must(IsAValidHttpsUrl).WithMessage("Original url must be a valid HTTPS URL.");
+        }
+        private bool IsAValidHttpsUrl(string url)
+        {
+            return Uri.TryCreate(url, UriKind.Absolute, out var uriResult) &&
+                   uriResult.Scheme == Uri.UriSchemeHttps;
+        }
+    }
+}
